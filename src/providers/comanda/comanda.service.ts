33a0d5eb95542451c751850444584fd856a2169a
Messias <messiasp_souza@outlook.com>
2019-02-10 01:11:20 -0200

    Revert "merge"
    This reverts commit 6c8b19fa16f3e526d67b709ef4504c104e290214.

--- src/providers/comanda/comanda.service.ts
+++ src/providers/comanda/comanda.service.ts
@@ -2,13 +2,20 @@ import { Injectable } from "@angular/core";
 
 import { AngularFireDatabase } from "angularfire2/database";
 import { Produto } from "../../model/produto/produto.model";
+<<<<<<< HEAD
 import { Comanda } from "../../model/comanda/comanda.model";
+=======
+>>>>>>> fa3172099f9e5f38574700680b42fc02e450747a
 
 
 @Injectable()
 export class ComandaService {
 
+<<<<<<< HEAD
   private comandaList = this.aFDatabase.list<Comanda>("/comandas");
+=======
+  private comandaList = this.aFDatabase.list<Produto>("/comanda");
+>>>>>>> fa3172099f9e5f38574700680b42fc02e450747a
 
   constructor( private aFDatabase: AngularFireDatabase ) {}
 
@@ -20,7 +27,11 @@ export class ComandaService {
     return this.aFDatabase;
   }
 
+<<<<<<< HEAD
   public async  createComanda( comanda: Comanda) {
+=======
+  public async createComanda( comanda: Produto) {
+>>>>>>> fa3172099f9e5f38574700680b42fc02e450747a
     console.log("Service, Obj ", comanda);
     // return await this.comandaList.push(comanda);
     return await this.comandaList.push(comanda)
